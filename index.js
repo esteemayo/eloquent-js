1f88dc53e0327e8ade691bdb5738b6fedb2778f1
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-11-12 03:28:50 +0100

    fix: destructuring

--- index.js
+++ index.js
@@ -306,3 +306,12 @@ console.log(Math.random());
 console.log(Math.floor(Math.random() * 10));
 console.log(Math.floor(4.9));
 console.log(Math.abs(-4.5));
+
+// destructuring
+
+const {name } = {
+  name: 'John',
+  age: 23
+}
+
+console.log(name);
